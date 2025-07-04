332bd8288f822d1ae2cd4fb67fbdc76e9d8101ac
bbyik-k <kby00517@naver.com>
2025-07-04 14:01:41 +0900

    feat(join-form): add uncontrolled input test with ref

--- react-join-form/src/components/JoinForm.tsx
+++ react-join-form/src/components/JoinForm.tsx
@@ -1,25 +1,29 @@
-import { useState, type FormEvent, type InputHTMLAttributes } from 'react';
+import { forwardRef, useRef, type FormEvent, type ForwardedRef, type InputHTMLAttributes } from 'react';
 
 export default function JoinForm() {
+  const nameRef = useRef<HTMLInputElement>(null);
+  const emailRef = useRef<HTMLInputElement>(null);
+  const phoneRef = useRef<HTMLInputElement>(null);
+
   const handleSubmit = (e: FormEvent<HTMLFormElement>) => {
     e.preventDefault();
     console.log({
-      name: (e.currentTarget.elements.namedItem('name') as HTMLInputElement).value,
-      email: (e.currentTarget.elements.namedItem('email') as HTMLInputElement).value,
-      phone: (e.currentTarget.elements.namedItem('phone') as HTMLInputElement).value,
+      name: (nameRef.current as HTMLInputElement).value,
+      email: (emailRef.current as HTMLInputElement).value,
+      phone: (phoneRef.current as HTMLInputElement).value,
     });
   };
 
   return (
     <form onSubmit={handleSubmit}>
       <div>
-        <Input id='name' name='name' type='text' required />
+        <Input id='name' name='name' type='text' required ref={nameRef} />
       </div>
       <div>
-        <Input id='email' name='email' type='email' required />
+        <Input id='email' name='email' type='email' required ref={emailRef} />
       </div>
       <div>
-        <Input id='phone' name='phone' type='tell' required />
+        <Input id='phone' name='phone' type='tell' required ref={phoneRef} />
       </div>
       <div>
         <button type='submit'>Join</button>
@@ -29,22 +33,12 @@ export default function JoinForm() {
   );
 }
 
-function Input({ id, name, ...props }: Omit<InputHTMLAttributes<HTMLInputElement>, 'value' | 'onChange'>) {
-  const [value, setValue] = useState('');
+const Input = forwardRef(function Input({ id, name, ...props }: InputHTMLAttributes<HTMLInputElement>, ref: ForwardedRef<HTMLInputElement>) {
   console.log('input', name);
   return (
     <>
       <label htmlFor={id}>{name}</label>
-      <input
-        id={id}
-        name={name}
-        {...props}
-        type='text'
-        value={value}
-        onChange={(e) => {
-          setValue(e.currentTarget.value);
-        }}
-      />
+      <input id={id} name={name} ref={ref} {...props} />
     </>
   );
-}
+});
