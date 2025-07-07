683ef149f372986f6f66087806b566c6886d5c5b
bbyik-k <kby00517@naver.com>
2025-07-07 10:42:18 +0900

    feat(join-form): apply lazy loading for performance test

--- react-join-form/src/App.tsx
+++ react-join-form/src/App.tsx
@@ -1,8 +1,14 @@
 import './App.css';
+import CompanySales from './components/CompanySales';
 import JoinForm from './components/JoinForm';
 
 function App() {
-  return <JoinForm />;
+  return (
+    <>
+      <JoinForm />
+      <CompanySales />
+    </>
+  );
 }
 
 export default App;
