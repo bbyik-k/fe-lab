2e7aa3e91743a2136c3e00541cf48bbbb2a1f215
bbyik-k <kby00517@naver.com>
2025-07-07 21:23:33 +0900

    feat(survey): add main layout and basic tab structure

--- multi-form-survey/src/App.tsx
+++ multi-form-survey/src/App.tsx
@@ -1,29 +1,22 @@
-import { useState } from 'react';
-import reactLogo from './assets/react.svg';
-import viteLogo from '/vite.svg';
 import './App.css';
+import MainLayout from './components/common/MainLayout';
+import Tabs, { Tab, TabList, TabPanel, TabPanels } from './components/common/Tabs';
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
     <>
-      <div className='bg-amber-400'>
-        <a href='https://vite.dev' target='_blank'>
-          <img src={viteLogo} className='logo' alt='Vite logo' />
-        </a>
-        <a href='https://react.dev' target='_blank'>
-          <img src={reactLogo} className='logo react' alt='React logo' />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className='card'>
-        <button onClick={() => setCount((count) => count + 1)}>count is {count}</button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className='read-the-docs'>Click on the Vite and React logos to learn more</p>
+      <MainLayout>
+        <Tabs>
+          <TabList>
+            <Tab index={0}>tab1</Tab>
+            <Tab index={1}>tab2</Tab>
+          </TabList>
+          <TabPanels>
+            <TabPanel index={0}>Panel1</TabPanel>
+            <TabPanel index={1}>Panel2</TabPanel>
+          </TabPanels>
+        </Tabs>
+      </MainLayout>
     </>
   );
 }
