6b53408aebcde6ca5cd9e149470232445b32fbca
bbyik-k <kby00517@naver.com>
2025-07-08 17:54:54 +0900

    feat(survey): add dropdown component

--- multi-form-survey/src/App.tsx
+++ multi-form-survey/src/App.tsx
@@ -1,4 +1,5 @@
 import './App.css';
+import Dropdown from './components/common/Dropdown';
 import MainLayout from './components/common/MainLayout';
 import Panel, { PanelBody, PanelCap, PanelFooter, PanelHeader } from './components/common/Panel';
 import Tabs, { Tab, TabList, TabPanel, TabPanels } from './components/common/Tabs';
@@ -16,7 +17,21 @@ function App() {
             <TabPanel index={0}>
               <PanelCap>2층 1섹션</PanelCap>
               <Panel>
-                <PanelHeader>Header</PanelHeader>
+                <PanelHeader>
+                  <>Header</>
+                  <Dropdown<string>
+                    options={[
+                      { label: <div>1</div>, value: '1' },
+                      { label: <div>2</div>, value: '2' },
+                      { label: <div>3</div>, value: '3' },
+                      { label: <div>4</div>, value: '4' },
+                      { label: <div>5</div>, value: '5' },
+                    ]}
+                    onChange={(value) => {
+                      console.log(value);
+                    }}
+                  />
+                </PanelHeader>
                 <PanelBody>Body</PanelBody>
                 <PanelFooter>Footer</PanelFooter>
               </Panel>
