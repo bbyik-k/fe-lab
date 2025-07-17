48cac6346c7876b3091f758c0bd32f471b1bd846
bbyik-k <kby00517@naver.com>
2025-07-17 10:36:21 +0900

    feat(todo-app): replace fetch with axios for data fetching

--- react-todo-app/src/api/users.ts
+++ react-todo-app/src/api/users.ts
@@ -1,6 +1,7 @@
 import type { User } from '../types';
+import AxiosInstance from './axiosInstance';
 
-const FETCH_URL = 'https://68760973814c0dfa653a55c2.mockapi.io/api/v1/user';
+// const FETCH_URL = 'https://68760973814c0dfa653a55c2.mockapi.io/api/v1/user';
 
 const refineUser = (users: User[]): User[] => {
   const refineData = users.map((user) => ({
@@ -13,12 +14,14 @@ const refineUser = (users: User[]): User[] => {
 //fetch 함수 작성
 export const fetchUsers = async (): Promise<User[]> => {
   try {
-    const response = await fetch(FETCH_URL);
-    if (!response.ok) {
-      throw new Error(`HTTP 오류, 상태: ${response.status}`);
-    }
-    const data: User[] = await response.json();
-    const refineData = refineUser(data);
+    // const response = await fetch(FETCH_URL);
+    const response = await AxiosInstance.get<User[]>('/user');
+    // if (!response.ok) {
+    //   throw new Error(`HTTP 오류, 상태: ${response.status}`);
+    // }
+    // const data: User[] = await response.json();
+    // const refineData = refineUser(data);
+    const refineData = refineUser(response.data);
     console.log(refineData);
     return refineData;
   } catch (error) {
